4b454c3202c61b353f6795cdfcc8b34f6bd2742b
Ryan Travers <rtravers@serlio.com>
2015-04-30 11:29:54 -0500

    Removed information
    Removed private information

--- Connections/homebrew.php
+++ Connections/homebrew.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_homebrew = "localhost";
-$database_homebrew = "rtravers_brews";
-$username_homebrew = "root";
-$password_homebrew = "beer4a11";
+$hostname_homebrew = "";
+$database_homebrew = "";
+$username_homebrew = "";
+$password_homebrew = "";
 $homebrew = mysql_pconnect($hostname_homebrew, $username_homebrew, $password_homebrew) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
