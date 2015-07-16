c0d69add5bc3369c7f0eb89f31f77eb34072046c
rtravers <rtravers@serlio.com>
2015-07-16 11:34:02 -0500

    Hops Table
    Added Table that includes all the extra bags of hops we have.

---
+++ hops.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<?php include_once 'head.php'; ?>
+<body>
+<?php
+include_once 'nav.php';
+?>
+<div class="container">
+    <div class="col-sm-6 col-sm-offset-3">
+        <div class='panel panel-default'>
+            <table class='table table-hover'>
+                <tr class='bg-primary'>
+                    <th>Hop Name</th>
+                    <th class="text-center">Ounces (oz)</th>
+                </tr>
+                <tr><td>Cascade</td><td class='text-center'>2.5</td></tr>
+                <tr><td>Centennial</td><td class='text-center'>2.5</td></tr>
+                <tr><td>Chinook</td><td class='text-center'>2</td></tr>
+                <tr><td>Columbus</td><td class='text-center'>3.5</td></tr>
+                <tr><td>French Aramis</td><td class='text-center'>2</td></tr>
+                <tr><td>German NB</td><td class='text-center'>1</td></tr>
+                <tr><td>German Tettnang</td><td class='text-center'>0.5</td></tr>
+                <tr><td>German Tradition</td><td class='text-center'>0.5</td></tr>
+                <tr><td>Hallertau</td><td class='text-center'>0.5</td></tr>
+                <tr><td>Hersbrucker</td><td class='text-center'>0.5</td></tr>
+                <tr><td>Magnum</td><td class='text-center'>2</td></tr>
+                <tr><td>Summit</td><td class='text-center'>2</td></tr>
+                <tr><td>UK Kent Golding</td><td class='text-center'>0.5</td></tr>
+                <tr><td>US Fuggle</td><td class='text-center'>1</td></tr>
+                <tr><td>US Willamette</td><td class='text-center'>1</td></tr>
+            </table>
+        </div>
+    </div>
+</div>
+</body>
+</html>
